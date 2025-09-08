a888db842bb94a0013e48d8166fba3979e16d8aa
BLESSEDSAMUELES <blessedsam40@gmail.com>
2025-09-08 14:24:51 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Test());
+}
+
+class Test extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 241, 247, 121),
+          title: Text('Quote App'),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25.0)),
+          ),
+        ),
+        drawer: Drawer(child: Center(child: Text('hello World'))),
+      ),
+    );
+  }
+}
